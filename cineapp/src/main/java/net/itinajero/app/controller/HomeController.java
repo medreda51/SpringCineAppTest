a33984d6dcc05f2bbf75bc6f8480cf3fa5511210
mohamed.hamdan <mohamed.hamdan@28APO4662.altran.es>
2018-06-04 13:13:25 +0200

    second commit

--- cineapp/src/main/java/net/itinajero/app/controller/HomeController.java
+++ cineapp/src/main/java/net/itinajero/app/controller/HomeController.java
@@ -2,45 +2,78 @@ package net.itinajero.app.controller;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import net.itinajero.app.model.Pelicula;
+import net.itinajero.app.util.Utileria;
 
 @Controller
 public class HomeController {
+	
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String goHome() {
 		return "home";
 	}
 
+	@RequestMapping(value="/search", method=RequestMethod.POST)
+	public String buscar(@RequestParam("fecha") String fecha, Model model){
+		
+		List<String> listaFechas = Utileria.getNextDays(4);
+		
+		List<Pelicula> peliculas = getLista();
+
+		model.addAttribute("fechas", listaFechas);
+		model.addAttribute("fechaBusqueda", fecha);
+		model.addAttribute("peliculas", peliculas);
+		
+		System.out.println("Buscando todas las peliculas en exhibicion para la fecha: " + fecha);
+		return "home";
+	}
+	
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String mostrarPrincipal(Model model) {
 
+		List<String> listaFechas = Utileria.getNextDays(4);
+		
 		List<Pelicula> peliculas = getLista();
 //		peliculas.add("Rapido y furioso");
 //		peliculas.add("El aro 2");
 //		peliculas.add("Aliens");
+		model.addAttribute("fechas", listaFechas);
+		model.addAttribute("fechaBusqueda", dateFormat.format(new Date()));
 		model.addAttribute("peliculas", peliculas);
 
 		return "home";
 	}
 
-	@RequestMapping(value = "/detail")
-	public String mostrarDetalle(Model model) {
-		String tituloPelicula = "Rapidos y furiosos";
-		int duracion = 136;
-		double precioEntrada = 50;
-
-		model.addAttribute("titulo", tituloPelicula);
-		model.addAttribute("duracion", duracion);
-		model.addAttribute("precio", precioEntrada);
+	//@RequestMapping(value = "/detail/{id}/{fecha}",method=RequestMethod.GET)	
+	@RequestMapping(value = "/detail",method=RequestMethod.GET)
+	//public String mostrarDetalle(Model model,@PathVariable("id") int idPelicula, @PathVariable("fecha") String fecha) {
+	public String mostrarDetalle(Model model,@RequestParam("idMovie") int idPelicula, @RequestParam("fecha") String fecha) {
+		
+		System.out.println("Buscando Horarios para la pelicula: " + idPelicula);
+		System.out.println("Para la fecha: " + fecha);
+		
+		// TODO - Buscar en la base de datos los horarios.		
+		
+//		String tituloPelicula = "Rapidos y furiosos";
+//		int duracion = 136;
+//		double precioEntrada = 50;
+//
+//		model.addAttribute("titulo", tituloPelicula);
+//		model.addAttribute("duracion", duracion);
+//		model.addAttribute("precio", precioEntrada);
 		return "detalle";
 	}
 
